d7145cbf23db62ae6e275ec77ca375e17aafdd2c
j-toscani <julian_toscani@gmx.de>
2020-09-12 11:28:25 +0200

    add boilerplate code

---
+++ index.js
@@ -0,0 +1,6 @@
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
