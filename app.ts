15f90e4326667d4c801f97a87012f83c2cbd94ce
Julian Toscani <julian_toscani@gmx.de>
2020-10-10 17:04:58 +0200

    add new files to try webhooks

--- app.ts
+++ app.ts
@@ -1,10 +1,24 @@
 import Telegraf from "telegraf";
 import dotenv from "dotenv";
+import fs from 'fs';
 import handleBahnCommand from "./commands/bahn";
 
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN || "", { username: "InfoBot" });
+
+const tlsOptions = {
+  key: fs.readFileSync('server-key.pem'),
+  cert: fs.readFileSync('server-cert.pem'),
+}
+
+bot.telegram.setWebhook('https://server.tld:8443/secret-path')
+
+// Start https webhook
+bot.startWebhook('/secret-path', tlsOptions, 8443)
+
+// Http webhook, for nginx/heroku users.
+bot.startWebhook('/secret-path', null, 5000)
 
 bot.start((ctx) => {
   const sender =
