4af9fb6768361450a57b47eee83b4c20125c8799
Julian Toscani <julian_toscani@gmx.de>
2020-11-03 19:20:16 +0000

    remove server set-up

--- app.ts
+++ app.ts
@@ -8,12 +8,8 @@ const expressApp = express()
 dotenv.config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN
-const PORT = process.env.PORT || 3000
-const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
 
 const bot = new Telegraf(process.env.BOT_TOKEN || "", { username: "InfoBot" });
-bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
-expressApp.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
 
 bot.start((ctx) => {
   const sender =
@@ -22,7 +18,7 @@ bot.start((ctx) => {
 });
 
 bot.help(ctx => {
-  ctx.reply(`Folgende Commandos stehen dir zur Verfügung: \n /bahn <start> <ziel>`);
+  ctx.reply(`Folgende Commandos stehen dir zur Verfügung: \n /bahn <start> <ziel> \n /roll <zahl>`);
 })
 
 bot.command("bahn", handleBahnCommand);
@@ -48,11 +44,3 @@ bot.command("roll", async function handleBahnCommand(ctx) {
 });
 
 bot.launch()
-
-// @ts-ignore
-expressApp.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-expressApp.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
